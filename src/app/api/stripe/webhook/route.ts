323ac569bbf4f5cf2de4a923e8c96406517bb9cb
pairardrones <you@example.com>
2026-03-09 00:00:49 -0300

    Add api ping route

--- src/app/api/stripe/webhook/route.ts
+++ src/app/api/stripe/webhook/route.ts
@@ -8,7 +8,7 @@ const supabase = createClient(
 )
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-02-24.acacia',
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
